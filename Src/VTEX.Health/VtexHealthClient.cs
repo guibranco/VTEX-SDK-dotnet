c5aa87243939bcbf0ff1f60b53985d8bdb1a275c
Guilherme Branco Stracini <guilherme@guilhermebranco.com.br>
2020-03-29 01:12:59 -0300

    Removed constructor with configuration

---
+++ Src/VTEX.Health/VtexHealthClient.cs
@@ -0,0 +1,49 @@
+﻿using Microsoft.Extensions.Logging;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Net.Http;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace VTEX.Health
+{
+    public class VtexHealthClient : IVtexHealthClient
+    {
+        /// <summary>
+        /// The logger
+        /// </summary>
+        private readonly ILogger<VtexHealthClient> _logger;
+
+        /// <summary>
+        /// The HTTP client
+        /// </summary>
+        private readonly HttpClient _httpClient;
+
+        #region ~ctors
+
+        public VtexHealthClient(ILoggerFactory loggerFactory, HttpClient httpClient)
+        {
+            if (loggerFactory == null)
+                throw new ArgumentNullException(nameof(loggerFactory));
+            _logger = loggerFactory.CreateLogger<VtexHealthClient>();
+            _httpClient = httpClient ?? throw new ArgumentNullException(nameof(httpClient));
+        }
+
+        #endregion
+
+        #region Implementation of IVtexHealthClient
+
+        /// <inheritdoc />
+        public async Task<IEnumerable<PlatformStatus>> GetPlatformStatuesAsync(CancellationToken cancellationToken)
+        {
+            _logger.LogDebug("Getting platform status");
+            var response = await _httpClient.GetAsync("/", cancellationToken);
+            var responseContent = await response.Content.ReadAsStringAsync();
+            _logger.LogDebug($"Platform status response: {response.StatusCode}");
+            return response.IsSuccessStatusCode ? JsonConvert.DeserializeObject<PlatformStatus[]>(responseContent) : default;
+        }
+
+        #endregion
+    }
+}
--- Src/VTEX/PlatformConstants.cs
+++ Src/VTEX/PlatformConstants.cs
@@ -9,63 +9,61 @@
         /// <summary>
         /// The rest oms feed
         /// </summary>
-        public const string OmsFeed = "oms/pvt/feed/orders/status/";
+        public const string OMS_FEED = "oms/pvt/feed/orders/status/";
 
         /// <summary>
         /// The rest oms orders
         /// </summary>
-        public const string OmsOrders = "oms/pvt/orders";
+        public const string OMS_ORDERS = "oms/pvt/orders";
 
         /// <summary>
         /// The rest oms invoices
         /// </summary>
-        public const string OmsInvoices = "oms/pub/orders";
+        public const string OMS_INVOICES = "oms/pub/orders";
 
         /// <summary>
         /// The rest oms tracking
         /// </summary>
-        public const string OmsTracking = "oms/pvt/orders/{0}/invoice/{1}/tracking";
+        public const string OMS_TRACKING = "oms/pvt/orders/{0}/invoice/{1}/tracking";
 
         /// <summary>
         /// The rest log warehouses
         /// </summary>
-        public const string LogWarehouses = "logistics/pvt/inventory/warehouseitembalances";
+        public const string LOG_WAREHOUSES = "logistics/pvt/inventory/warehouseitembalances";
 
         /// <summary>
         /// The rest pci transactions
         /// </summary>
-        public const string PciTransactions = "pvt/transactions";
+        public const string PCI_TRANSACTIONS = "pvt/transactions";
 
         /// <summary>
         /// The rest log reservations
         /// </summary>
-        public const string LogReservations = "logistics/pvt/inventory/reservations";
+        public const string LOG_RESERVATIONS = "logistics/pvt/inventory/reservations";
 
         /// <summary>
         /// The rest log inventory
         /// </summary>
-        public const string LogInventory = "logistics/pvt/inventory/skus";
+        public const string LOG_INVENTORY = "logistics/pvt/inventory/skus";
 
         /// <summary>
         /// The rest bridge search
         /// </summary>
-        public const string BridgeSearch = "bridge/mb/search";
+        public const string BRIDGE_SEARCH = "bridge/mb/search";
 
         /// <summary>
         /// The pricing endpoint.
         /// </summary>
-        public const string Pricing = "pricing/prices";
+        public const string PRICING = "pricing/prices";
 
         /// <summary>
         /// The rest catalog
         /// </summary>
-        public const string Catalog = "catalog_system/pvt";
+        public const string CATALOG = "catalog_system/pvt";
 
         /// <summary>
         /// The rest catalog pub
         /// </summary>
-        public const string CatalogPub = "catalog_system/pub";
-
-
+        public const string CATALOG_PUB = "catalog_system/pub";
     }
 }
