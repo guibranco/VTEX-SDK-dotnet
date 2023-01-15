88ea0804600475bbd3d363e4b8034166b27fc445
Guilherme Branco Stracini <guilherme@guilhermebranco.com.br>
2023-01-16 01:38:00 +0400

    Add invoiceData.cs

---
+++ Src/VTEX/Transport/InvoiceData.cs
@@ -0,0 +1,30 @@
+﻿// ***********************************************************************
+// Assembly         : VTEX
+// Author           : Guilherme Branco Stracini
+// Created          : 01-16-2023
+//
+// Last Modified By : Guilherme Branco Stracini
+// Last Modified On : 01-16-2023
+// ***********************************************************************
+// <copyright file="InvoiceData.cs" company="Guilherme Branco Stracini">
+//     © 2020 Guilherme Branco Stracini. All rights reserved.
+// </copyright>
+// <summary></summary>
+// ***********************************************************************
+namespace VTEX.Transport
+{
+    using Newtonsoft.Json;
+
+    /// <summary>
+    /// Class InvoiceData.
+    /// </summary>
+    public class InvoiceData
+    {
+        /// <summary>
+        /// Gets or sets the address.
+        /// </summary>
+        /// <value>The address.</value>
+        [JsonProperty("address")]
+        public string Address { get; set; }
+    }
+}
