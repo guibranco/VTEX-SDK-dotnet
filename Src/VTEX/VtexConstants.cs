8f1a356e779bfce0e42a1c32ae697b7e36fc458d
Guilherme Branco Stracini <guilherme@guilhermebranco.com.br>
2023-01-16 02:52:51 +0400

    Feature/new entities (#84)
    * Add invoiceData.cs
    
    * Update Installments
    
    * Update AddicitonaInfo.cs
    
    * Rename BankBillSettlement to BankSplitSettlement
    
    * Update docs
    
    * Add BridgeItem.cs
    
    * Fix change date
    
    * Fix change date
    
    * Update date
    
    * Updates
    
    * Fix docs
    
    * Add file header
    
    * Add file header
    
    * Move bridge items
    
    * Update old code
    
    * FIx PCIPayment
    
    * Remove blank lines
    
    * Add constants
    
    * Add constants

---
+++ Src/VTEX/VtexConstants.cs
@@ -0,0 +1,52 @@
+﻿// ***********************************************************************
+// Assembly         : 
+// Author           : Guilherme Branco Stracini
+// Created          : 01-16-2023
+//
+// Last Modified By : Guilherme Branco Stracini
+// Last Modified On : 01-16-2023
+// ***********************************************************************
+// <copyright file="VTEXConstants.cs" company="Guilherme Branco Stracini ME">
+//     Copyright (c) Guilherme Branco Stracini ME. All rights reserved.
+// </copyright>
+// <summary></summary>
+// ***********************************************************************
+namespace VTEX
+{
+    /// <summary>
+    /// Class VTEX Constants.
+    /// </summary>
+    //TODO replace public to internal after remove the old code from Integração Service.
+    public static class VTEXConstants
+    {
+        /// <summary>
+        /// The platform stable domain
+        /// </summary>
+        public const string PlatformStableDomain = "vtexcommercestable.com.br";
+        /// <summary>
+        /// The payments domain
+        /// </summary>
+        public const string PaymentsDomain = "vtexpayments.com.br";
+        /// <summary>
+        /// The logistics domain
+        /// </summary>
+        public const string LogisticsDomain = "logistics.vtexcommercestable.com.br";
+        /// <summary>
+        /// The API domain
+        /// </summary>
+        public const string ApiDomain = "api.vtex.com";
+        /// <summary>
+        /// My vtex domain
+        /// </summary>
+        public const string MyVtexDomain = "myvtex.com";
+        /// <summary>
+        /// The monitoring domain
+        /// </summary>
+        public const string MonitoringDomain = "monitoring.vtex.com";
+
+        /// <summary>
+        /// The vtex identifier client authentication cookie name
+        /// </summary>
+        public const string VtexIdClientAuthCookieName = "VtexIdclientAutCookie";
+    }
+}
