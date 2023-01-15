047d4adff371635554a31c8191ca8128c7f242dc
Guilherme Branco Stracini <guilherme@guilhermebranco.com.br>
2023-01-16 02:15:45 +0400

    Add file header

--- Src/VTEX/Transport/CourierStatus.cs
+++ Src/VTEX/Transport/CourierStatus.cs
@@ -1,4 +1,17 @@
-﻿namespace VTEX.Transport
+﻿// ***********************************************************************
+// Assembly         : VTEX
+// Author           : Guilherme Branco Stracini
+// Created          : 01-15-2023
+//
+// Last Modified By : Guilherme Branco Stracini
+// Last Modified On : 01-16-2023
+// ***********************************************************************
+// <copyright file="CourierStatus.cs" company="Guilherme Branco Stracini">
+//     © 2020 Guilherme Branco Stracini. All rights reserved.
+// </copyright>
+// <summary></summary>
+// ***********************************************************************
+namespace VTEX.Transport
 {
     using System;
     using Newtonsoft.Json;
--- Src/VTEX/Transport/CustomApp.cs
+++ Src/VTEX/Transport/CustomApp.cs
@@ -1,4 +1,17 @@
-﻿namespace VTEX.Transport
+﻿// ***********************************************************************
+// Assembly         : VTEX
+// Author           : Guilherme Branco Stracini
+// Created          : 01-16-2023
+//
+// Last Modified By : Guilherme Branco Stracini
+// Last Modified On : 01-16-2023
+// ***********************************************************************
+// <copyright file="CustomApp.cs" company="Guilherme Branco Stracini">
+//     © 2020 Guilherme Branco Stracini. All rights reserved.
+// </copyright>
+// <summary></summary>
+// ***********************************************************************
+namespace VTEX.Transport
 {
     /// <summary>
     /// Class CustomApp.
