458684298369232a4f68c9719348823f2d6105fa
Guilherme Branco Stracini <guilherme@guilhermebranco.com.br>
2024-10-17 15:07:17 +0100

    Feature/19 feature add documentation using docusaurus and GitHub pages (#321)
    * Create deploy-docs.yml
    
    * Create package.json
    
    * Create index.js
    
    * Create docusaurus.config.js
    
    * Update infisical-secrets-check.yml

---
+++ docs/src/pages/index.js
@@ -0,0 +1,18 @@
+
+import React from 'react';
+import Layout from '@theme/Layout';
+
+function Home() {
+  return (
+    <Layout
+      title="VTEX SDK Documentation"
+      description="Comprehensive documentation for VTEX SDK">
+      <main>
+        <h1>Welcome to VTEX SDK Documentation</h1>
+        <p>Explore the documentation to understand how to use the VTEX SDK effectively.</p>
+      </main>
+    </Layout>
+  );
+}
+
+export default Home;
