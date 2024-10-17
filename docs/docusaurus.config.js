2e828b2d34fd86f5419287d3978a9c8fef619ce5
Guilherme Branco Stracini <guilherme@guilhermebranco.com.br>
2024-10-17 14:47:13 +0100

    Create docusaurus.config.js

---
+++ docs/docusaurus.config.js
@@ -0,0 +1,23 @@
+module.exports = {
+  title: 'VTEX SDK',
+  tagline: 'Documentation for VTEX SDK',
+  url: 'https://guibranco.github.io',
+  baseUrl: '/VTEX-SDK-dotnet/',
+  onBrokenLinks: 'throw',
+  onBrokenMarkdownLinks: 'warn',
+  favicon: 'img/favicon.ico',
+  organizationName: 'guibranco',
+  projectName: 'VTEX-SDK-dotnet',
+  themeConfig: {
+    navbar: {
+      title: 'VTEX SDK',
+      logo: {
+        alt: 'VTEX Logo',
+        src: 'img/logo.svg',
+      },
+      items: [
+        {to: 'docs/', label: 'Docs', position: 'left'},
+      ],
+    },
+  },
+};
