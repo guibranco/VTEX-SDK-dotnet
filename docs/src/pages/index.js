dd001be5f19439427633718dd69c366e29c0a6d1
Guilherme Branco Stracini <guilherme@guilhermebranco.com.br>
2024-11-12 20:10:39 +0000

    Update index.js

--- docs/src/pages/index.js
+++ docs/src/pages/index.js
@@ -1,33 +1,14 @@
-
 import React from 'react';
 import Layout from '@theme/Layout';
 
-/**
- * Renders the main component for the VTEX SDK Documentation.
- *
- * This function returns a layout component that includes a title and description
- * for the documentation, along with a welcome message and introductory text.
- *
- * @returns {JSX.Element} A JSX element representing the layout of the documentation page.
- *
- * @example
- * // Usage within a React component
- * const App = () => {
- *   return (
- *     <div>
- *       <Home />
- *     </div>
- *   );
- * };
- */
 function Home() {
   return (
     <Layout
-      title="VTEX SDK Documentation"
-      description="Comprehensive documentation for VTEX SDK">
+      title="VTEX SDK .NET Documentation"
+      description="Comprehensive documentation for Sankhya SDK .NET">
       <main>
         <h1>Welcome to VTEX SDK Documentation</h1>
-        <p>Explore the documentation to understand how to use the VTEX SDK effectively.</p>
+        <p>Explore the documentation to understand how to use the VTEX SDK dotNET effectively.</p>
       </main>
     </Layout>
   );
